5c3896dde04af1dcfd7be57dddc5ce3c87ef4450
Braian Rios <braian.rios@ing.austral.edu.ar>
2026-02-04 15:49:11 -0300

    fix(spike-2): add DatabaseModule for Hilt DI + configure HiltWorkerFactory

--- app/src/main/java/com/brios/miempresa/MiEmpresa.kt
+++ app/src/main/java/com/brios/miempresa/MiEmpresa.kt
@@ -1,7 +1,18 @@
 package com.brios.miempresa
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MiEmpresa : Application()
+class MiEmpresa : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: androidx.hilt.work.HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() =
+            Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
+}

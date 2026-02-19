f2de0574052bcae460876851e883f157c4e000c1
Braian Rios <braian.rios@ing.austral.edu.ar>
2026-02-19 09:52:17 -0300

    chore(release): bump app version to 2.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "com.brios.miempresa"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.1.2"
+        versionCode = 6
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
